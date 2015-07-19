0a2e72558ff138599809289536116ba2d6a5315a
Hongbo Jing <jack.jing7@gmail.com>
2015-07-19 02:45:47 -0400

    day23 version 1 scripting dom exercise everyday

--- builds/development/js/lib/app.js
+++ builds/development/js/lib/app.js
@@ -1,3 +1,15 @@
-console.log(typeof document.getElementById('purchases'));
+//1 loop through the array that the .getElementByTagName returns
+//BP -> var liItem=document.getElementsByTagName("li");
 
-console.log(document.getElementsByTagName('li').length);
+var liItem=document.getElementsByTagName("li");
+for(var i=0;i<liItem.length;i++){
+  console.log(typeof liItem[i]);
+}
+//2 use wild character to display the total number of the nods in a document
+var totalNodeArray=document.getElementsByTagName("*");
+console.log('2 node total number is '+totalNodeArray.length);
+
+//3 combine getElementById and getElementByTagName to get the total number of nodes inside of a element whose id is 'purchase'
+var purchaseNode=document.getElementById("purchase");
+var nodesInPurchase=purchaseNode.getElementsByTagName("*");
+console.log('3 node total number in #purchase is '+nodesInPurchase.length);
